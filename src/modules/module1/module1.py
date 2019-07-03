ebbc5c9752d592324c86aa2f1c99faf3d48b5e76
Tejas-26 <tejas.chandrasekar@holmusk.com>
2019-07-03 16:07:50 +0800

    This is the first commit

---
+++ src/modules/module1/module1.py
@@ -0,0 +1,54 @@
+from logs import logDecorator as lD 
+import jsonref, pprint
+
+config = jsonref.load(open('../config/config.json'))
+logBase = config['logging']['logBase'] + '.modules.module1.module1'
+
+
+@lD.log(logBase + '.doSomething')
+def doSomething(logger):
+    '''print a line
+    
+    This function simply prints a single line
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger used for logging error information
+    '''
+
+    print('We are in module 1')
+
+    return
+
+@lD.log(logBase + '.main')
+def main(logger, resultsDict):
+    '''main function for module1
+    
+    This function finishes all the tasks for the
+    main function. This is a way in which a 
+    particular module is going to be executed. 
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger used for logging error information
+    resultsDict: {dict}
+        A dintionary containing information about the 
+        command line arguments. These can be used for
+        overwriting command line arguments as needed.
+    '''
+
+    print('='*30)
+    print('Main function of module 1')
+    print('='*30)
+    print('We get a copy of the result dictionary over here ...')
+    pprint.pprint(resultsDict)
+
+    doSomething()
+
+    print('Getting out of Module 1')
+    print('-'*30)
+
+    return
+
